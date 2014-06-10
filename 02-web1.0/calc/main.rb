6cb576ddf6260abd05ccdfa74726754f96034225
Cold Head <certainthings@gmail.com>
2014-06-10 12:30:19 +1000

    style

--- 02-web1.0/calc/main.rb
+++ 02-web1.0/calc/main.rb
@@ -4,5 +4,16 @@ require 'sinatra/reloader'
 
 get '/calc/multiply/:x/:y' do
   @result = params[:x].to_f * params[:y].to_f
+  @operation = '*'
   erb :calc
+end
+
+get '/calc/add/:x/:y' do
+  @result = params[:x].to_f + params[:y].to_f
+  @operation = '+'
+  erb :calc
+end
+
+get '/about' do
+  erb :about
 end
