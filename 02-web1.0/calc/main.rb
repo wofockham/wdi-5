1bb2f9fc178165ab00d9dfcdec701d232ca76898
Cold Head <certainthings@gmail.com>
2014-06-10 12:59:06 +1000

    calc with nav

--- 02-web1.0/calc/main.rb
+++ 02-web1.0/calc/main.rb
@@ -2,6 +2,24 @@ require 'pry'
 require 'sinatra'
 require 'sinatra/reloader'
 
+get '/calc' do
+  erb :form
+end
+
+get '/results' do
+  @first = params[:first].to_f
+  @second = params[:second].to_f
+
+  @result = case params[:operator]
+  when '+' then @first + @second
+  when '-' then @first - @second
+  when '*' then @first * @second
+  when '/' then @first / @second
+  end
+
+  erb :calc
+end
+
 get '/calc/multiply/:x/:y' do
   @result = params[:x].to_f * params[:y].to_f
   @operation = '*'
