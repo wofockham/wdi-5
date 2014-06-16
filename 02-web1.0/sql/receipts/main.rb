0da9d770b9c0da3ce59eb19693fb40d59be18a9f
Cold Head <certainthings@gmail.com>
2014-06-16 16:23:13 +1000

    SQL interpolation string

--- 02-web1.0/sql/receipts/main.rb
+++ 02-web1.0/sql/receipts/main.rb
@@ -14,3 +14,29 @@ end
 get '/new' do
   erb :new_receipt
 end
+
+post '/' do
+  db = SQLite3::Database.new "receipts.db"
+  db.results_as_hash = true
+
+      # "INSERT INTO receipts (service, cost) VALUES ('Do some service', '12')"
+  sql = "INSERT INTO receipts (service, cost) VALUES ('#{ params['service'] }', '#{ params['cost'] }')"
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
