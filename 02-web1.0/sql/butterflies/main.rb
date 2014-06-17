b58bb68cf3796b238def4db4ff20d3c687a84528
Cold Head <certainthings@gmail.com>
2014-06-17 11:56:01 +1000

    basic schema

---
+++ 02-web1.0/sql/butterflies/main.rb
@@ -0,0 +1,45 @@
+require 'pry'
+require 'sinatra'
+require 'sinatra/reloader'
+
+require 'sqlite3'
+
+get '/' do
+  redirect to '/butterflies'
+end
+
+# All butterflies
+get '/butterflies' do
+end
+
+# A specific butterfly
+get '/butterflies/:id' do
+end
+
+# Edit a butterfly
+get '/butterflies/:id/edit' do
+end
+
+# Update a butterfly
+post '/butterflies/:id' do
+end
+
+# Delete a butterfly
+get '/butterflies/:id/delete' do
+end
+
+# Form for a new butterfly
+get '/butterflies/new' do
+end
+
+# Add a new buttefly to the database
+post '/butterflies' do
+end
+
+def query_db(sql)
+  db = SQLite3::Database.new "butterflies.db"
+  db.results_as_hash = true
+  result = db.execute sql
+  db.close
+  result
+end
