75a06fc8c56bf590147a29d23d09334d69c90fbd
Cold Head <certainthings@gmail.com>
2014-06-19 16:21:11 +1000

    bored now

--- 02-web1.0/sql/bowling/models/frame.rb
+++ 02-web1.0/sql/bowling/models/frame.rb
@@ -5,11 +5,19 @@ class Frame < ActiveRecord::Base
     first_bowl.to_i + second_bowl.to_i
   end
 
+  def pins_remaining
+    10 - (first_bowl.to_i) - (second_bowl.to_i)
+  end
+
   def completed?
     !! (score == 10 || (first_bowl && second_bowl))
   end
 
+  def strike?
+    10 == first_bowl.to_i
+  end
+
   def spare?
-    10 == first_bowl.to_i + second_bowl.to_i
+    second_bowl && (10 == first_bowl.to_i + second_bowl.to_i)
   end
 end
--- 02-web1.0/sql/bowling/models/game.rb
+++ 02-web1.0/sql/bowling/models/game.rb
@@ -1,10 +1,27 @@
 class Game < ActiveRecord::Base
   has_many :frames
 
+  def score
+    sum = 0
+    frames.each do |f|
+      sum += f.score
+    end
+
+    sum
+  end
+
   def bowl(pins)
     self.save unless self.id
 
     current = current_frame
+
+    if frames.length >= 10 && current.completed?
+      raise "Game over"
+    end
+
+    if pins > (10 - current.first_bowl.to_i)
+      raise "Too many pins"
+    end
 
     if (current.first_bowl)
       current.second_bowl = pins
