27d6f1c45c15a2f3c2afba713546e3e3a8e8d76f
Cold Head <certainthings@gmail.com>
2014-06-24 09:54:38 +1000

    text helpers

--- 03-rails/helpers/app/controllers/pages_controller.rb
+++ 03-rails/helpers/app/controllers/pages_controller.rb
@@ -4,11 +4,15 @@ class PagesController < ApplicationController
 
   def numbers
     @amount = 234.0123456
-    @large_number = 23912834198
+    @large_number = 2123434983343948
     @phone = 2125551212
   end
 
   def text
+    @numbers = (1..20).to_a
+    @person_count = 1
+    @people_count = 2
+    @story = "A long time ago in a galaxy far far away"
   end
 
   def assets
