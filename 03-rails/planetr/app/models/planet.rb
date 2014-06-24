9d55164e0774e13289e27bd1380ca8cf2b151a4b
Cold Head <certainthings@gmail.com>
2014-06-24 11:44:21 +1000

    migration

--- 03-rails/planetr/app/models/planet.rb
+++ 03-rails/planetr/app/models/planet.rb
@@ -2,13 +2,15 @@
 #
 # Table name: planets
 #
-#  id       :integer          not null, primary key
-#  name     :string(255)
-#  image    :text
-#  orbit    :float
-#  diameter :float
-#  mass     :float
-#  moons    :integer
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  image      :text
+#  orbit      :float
+#  mass       :float
+#  diameter   :float
+#  moons      :integer
+#  created_at :datetime
+#  updated_at :datetime
 #
 
 class Planet < ActiveRecord::Base
