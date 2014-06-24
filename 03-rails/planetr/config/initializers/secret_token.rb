6db777a0ff4352fb3603c7db9cad0a64a1562867
Cold Head <certainthings@gmail.com>
2014-06-24 10:55:47 +1000

    planets database

---
+++ 03-rails/planetr/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Planetr::Application.config.secret_key_base = '32602be8f7c98d33c469b21d1b46617ce787ee2a90cee9a0676e9478a619b6bd54ebafa3d2db444d349c4612adb39527b4919750ae9810e82cd28e373fb38864'
---
+++ 03-rails/planetr/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Planetr::Application.config.session_store :cookie_store, key: '_planetr_session'
