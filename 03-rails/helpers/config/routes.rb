62f9ad4365967d20a9989864c653cf1fa768c671
Cold Head <certainthings@gmail.com>
2014-06-24 10:17:16 +1000

    url helpers

--- 03-rails/helpers/config/routes.rb
+++ 03-rails/helpers/config/routes.rb
@@ -1,6 +1,6 @@
 Helpers::Application.routes.draw do
   root :to => 'pages#home'
-  get '/numbers' => 'pages#numbers'
+  get '/numbers_are_amazing' => 'pages#numbers', :as => :numbers
   get '/text' => 'pages#text'
   get '/assets_demo' => 'pages#assets'
   get '/url' => 'pages#url'
