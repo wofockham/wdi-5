2c77fd1de836b053e1d968f24ce5e8b388260d50
Cold Head <certainthings@gmail.com>
2014-06-24 14:23:59 +1000

    planets show page

--- 03-rails/planetr/app/controllers/planets_controller.rb
+++ 03-rails/planetr/app/controllers/planets_controller.rb
@@ -9,7 +9,11 @@ class PlanetsController < ApplicationController
   end
 
   def create
-    planet = Planet.create params[:planet]
+    Planet.create params.require(:planet).permit(:name, :image, :orbit, :diameter, :mass, :moons)
     redirect_to planets_path
+  end
+
+  def show
+    @planet = Planet.find params[:id]
   end
 end
--- 03-rails/planetr/config/routes.rb
+++ 03-rails/planetr/config/routes.rb
@@ -2,4 +2,5 @@ Planetr::Application.routes.draw do
   get '/planets' => 'planets#index'
   get '/planets/new' => 'planets#new'
   post '/planets' => 'planets#create'
+  get '/planets/:id' => 'planets#show'
 end
