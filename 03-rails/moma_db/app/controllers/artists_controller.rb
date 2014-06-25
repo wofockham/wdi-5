b78ad8b7b43b29a3ec7ab96562d931823322cbb2
Cold Head <certainthings@gmail.com>
2014-06-25 10:42:03 +1000

    artist index

---
+++ 03-rails/moma_db/app/controllers/artists_controller.rb
@@ -0,0 +1,23 @@
+class ArtistsController < ApplicationController
+  def index
+    @artists = Artist.all
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- 03-rails/moma_db/config/routes.rb
+++ 03-rails/moma_db/config/routes.rb
@@ -1,56 +1,20 @@
 MomaDb::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  resources :artists, :works
 end
+
+#     artists GET    /artists(.:format)          artists#index
+#             POST   /artists(.:format)          artists#create
+#  new_artist GET    /artists/new(.:format)      artists#new
+# edit_artist GET    /artists/:id/edit(.:format) artists#edit
+#      artist GET    /artists/:id(.:format)      artists#show
+#             PATCH  /artists/:id(.:format)      artists#update
+#             PUT    /artists/:id(.:format)      artists#update
+#             DELETE /artists/:id(.:format)      artists#destroy
+#       works GET    /works(.:format)            works#index
+#             POST   /works(.:format)            works#create
+#    new_work GET    /works/new(.:format)        works#new
+#   edit_work GET    /works/:id/edit(.:format)   works#edit
+#        work GET    /works/:id(.:format)        works#show
+#             PATCH  /works/:id(.:format)        works#update
+#             PUT    /works/:id(.:format)        works#update
+#             DELETE /works/:id(.:format)        works#destroy
