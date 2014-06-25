a905ea132890a68ef96d16140473be6409dcedfa
Cold Head <certainthings@gmail.com>
2014-06-25 10:19:13 +1000

    seed

---
+++ 03-rails/moma_db/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MomaDb::Application.config.secret_key_base = '2adb7cb4d2358dd56b9c053df9e7b144bde0fef11848c67c6210630f1654a6bf001d1f3a4338249cd4b6ffaf5bae32a2e17c8bc216cf1975d0275a8b1bfd96ae'
---
+++ 03-rails/moma_db/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MomaDb::Application.config.session_store :cookie_store, key: '_moma_db_session'
