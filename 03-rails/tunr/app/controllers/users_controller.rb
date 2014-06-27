aab55368891c7682e63ea9725e6b75ae5ecdfaff
Cold Head <certainthings@gmail.com>
2014-06-27 12:40:15 +1000

    validations

--- 03-rails/tunr/app/controllers/users_controller.rb
+++ 03-rails/tunr/app/controllers/users_controller.rb
@@ -4,6 +4,16 @@ class UsersController < ApplicationController
   end
 
   def create
-    raise params.inspect
+    @user = User.new user_params
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :avatar, :password, :password_confirmation)
   end
 end
--- 03-rails/tunr/app/models/user.rb
+++ 03-rails/tunr/app/models/user.rb
@@ -13,4 +13,6 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :mixtapes
+  validates :avatar, :presence => true
+  validates :username, :presence => true, :length => { :minimum => 2 }
 end
