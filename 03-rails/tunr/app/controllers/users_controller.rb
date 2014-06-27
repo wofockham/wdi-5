44d1841778ee748f9220ba1a75506877d36f0a76
Cold Head <certainthings@gmail.com>
2014-06-27 15:43:16 +1000

    users#index

--- 03-rails/tunr/app/controllers/users_controller.rb
+++ 03-rails/tunr/app/controllers/users_controller.rb
@@ -12,6 +12,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @users = User.all
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :avatar, :password, :password_confirmation)
