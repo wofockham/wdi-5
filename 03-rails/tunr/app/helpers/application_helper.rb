c68e209d36fde676a6e4a35053578e27c8d5fdd0
Cold Head <certainthings@gmail.com>
2014-06-27 15:14:59 +1000

    smartnav

--- 03-rails/tunr/app/helpers/application_helper.rb
+++ 03-rails/tunr/app/helpers/application_helper.rb
@@ -1,5 +1,12 @@
 module ApplicationHelper
   def smartnav
-    "<li>Cool beans</li>"
+    if @current_user.present?
+      "<li>" +
+       link_to('Logout ' + @current_user.username, login_path, :data => {:method => :delete, :confirm => 'Really logout?'}) +
+       "</li>"
+    else
+      "<li><%= link_to('Sign up', new_user_path) %></li>
+      <li><%= link_to('Sign in', login_path) %></li>"
+    end
   end
 end
