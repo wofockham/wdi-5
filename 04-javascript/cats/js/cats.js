90eb762156917cc3359d03dee62ea81152d1dc28
Cold Head <certainthings@gmail.com>
2014-07-08 12:59:57 +1000

    cat factory

--- 04-javascript/cats/js/cats.js
+++ 04-javascript/cats/js/cats.js
@@ -1,16 +1,32 @@
-var lizzieTheCat = {
-  age: 18,
-  furColor: 'grey',
-  meow: function() {
-    console.log('meowww');
-  },
-  eat: function(food) {
-    console.log('Yum, I love ' + food);
-  },
-  sleep: function(numMinutes) {
-    for (var i = 0; i < numMinutes; i++) {
-      console.log('z');
+var catFactory = function (ageInYears, color, religion) {
+  var cat = {
+    age: ageInYears,
+    furColor: color,
+    religion: religion,
+    meow: function() {
+      console.log('meowww');
+    },
+    eat: function(food) {
+      console.log('Yum, I merely tolerate ' + food);
+    },
+    sleep: function(numMinutes) {
+      for (var i = 0; i < numMinutes; i++) {
+        console.log('z');
+      }
+    },
+    destroyFurniture: function () {
+      console.log('Muahahaha');
     }
-  }
+  };
+
+  return cat;
 };
 
+var syntaxTheCat = catFactory(25, 'black and white tuxedo', 'Satanic');
+
+var lizzieTheCat = catFactory();
+lizzieTheCat.age = 18;
+lizzieTheCat.furColor = 'grey';
+lizzieTheCat.religion = 'Quaker';
+
+var daemonTheCat = catFactory(1, 'black');
