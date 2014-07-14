cfd296d8d4cf51200d36fa0f624c0d83956c82d5
Cold Head <certainthings@gmail.com>
2014-07-14 15:51:08 +1000

    painting

--- 04-javascript/paintr/app/assets/javascripts/pages.js
+++ 04-javascript/paintr/app/assets/javascripts/pages.js
@@ -9,9 +9,50 @@ $(document).ready(function () {
   };
 
   var set_color = function () {
-    alert("you clicked a color");
+    var $box = $(this);
+    var color = $box.css('background-color');
+    $('#selected').css('background-color', color);
+  };
+
+  var clear_colors = function () {
+    $('#colors').empty();
+    $('#selected').css('background-color', 'white');
+  };
+
+  var paint = function () {
+    var color = $('#selected').css('background-color');
+    var $pixel = $(this);
+    $pixel.css('background-color', color);
   };
 
   $('#add_color').on('click', add_color);
-  $('.box').on('click', set_color);
+  $('#clear').on('click', clear_colors);
+  $('#colors').on('click', '.box', set_color); // Delegation!
+  $('#canvas').on('mouseover', '.pixel', paint); // Delegation!
+
+  var create_pixels = function () {
+    var $canvas = $('#canvas'); // Cache this to prevent repeatedly querying for it.
+    _(5000).times(function () {
+      var $pixel = $('<div/>');
+      $pixel.addClass('pixel');
+      $pixel.appendTo($canvas);
+    });
+  };
+
+  create_pixels();
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
