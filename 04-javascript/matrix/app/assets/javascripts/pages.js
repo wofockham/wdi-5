dbd8640e9ff70fe50dad289b68d5bb0f82a5c1af
Cold Head <certainthings@gmail.com>
2014-07-14 12:55:17 +1000

    underscore

--- 04-javascript/matrix/app/assets/javascripts/pages.js
+++ 04-javascript/matrix/app/assets/javascripts/pages.js
@@ -1,18 +1,52 @@
-$(document).ready(function () {
-  var numbers = [];
+var numbers = [];
 
+
+$(document).ready(function () {
   var add_number = function () {
     var number = parseInt($('#number').val());
 
     numbers.push(number);
 
-    // jQuery can create new elements on the fly, instead of selecting them.
+    add_box(number);
+
+    $('#number').val('').focus();
+  };
+
+  var square = function () {
+    clear_boxes();
+    numbers = _(numbers).map(function (n) {
+      return n * n;
+    });
+    show_boxes();
+  };
+
+  var clear_boxes = function () {
+    $('#boxes').empty();
+  };
+
+  var show_boxes = function () {
+    _(numbers).each(function (n) {
+      add_box(n);
+    });
+  };
+
+  var add_box = function (n) {
     var $box = $('<div></div>');
     $box.addClass('box');
-    $box.text(number);
-
+    $box.text(n);
     $('#boxes').prepend($box);
   };
 
   $('#add_number').click(add_number);
+  $('#square').click(square);
 });
+
+
+
+
+
+
+
+
+
+
