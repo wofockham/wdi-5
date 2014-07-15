59e49915b173643386319e647d407222c6da90c6
Cold Head <certainthings@gmail.com>
2014-07-15 12:14:34 +1000

    login

---
+++ 04-javascript/todor/app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.where(:email => params[:email]).first
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to(root_path)
+    else
+      redirect_to(login_path)
+    end
+  end
+
+  def destroy
+  end
+end
