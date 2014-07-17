4035906fad4a9221c2aec37e8ec947d202de7bc2
Cold Head <certainthings@gmail.com>
2014-07-17 15:39:27 +1000

    hello maps

--- 04-javascript/todor/config/routes.rb
+++ 04-javascript/todor/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root :to => 'users#index'
   resources :users, :only => [:index, :new, :create]
   resources :priorities, :only => [:index, :create]
+  resources :tasks, :only => [:index]
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
