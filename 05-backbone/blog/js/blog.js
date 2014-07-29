11f8c7b7786fe147478c5e4b9c48cdecc13407cf
Cold Head <certainthings@gmail.com>
2014-07-29 14:20:36 +1000

    posts collection

--- 05-backbone/blog/js/blog.js
+++ 05-backbone/blog/js/blog.js
@@ -7,8 +7,12 @@ app.Router = Backbone.Router.extend({
   },
   initialize: function () {
     console.log('router initialized');
-    var p = new app.Post({id: 1, title: 'First post', content: 'Backbone is hell'});
-    console.log(p);
+    app.posts = new app.Posts([
+      new app.Post({id: 1, title: 'First post', content: 'Backbone is hell'}),
+      new app.Post({id: 2, title: 'Second post', content: 'Backbone is slightly hellish'}),
+      new app.Post({id: 3, title: 'Third post', content: 'Backbone is getting better'}),
+      new app.Post({id: 4, title: 'Fourth post', content: 'Backbone is okay'})
+    ]);
   },
   index: function () {
     console.log('you reached index');
@@ -26,6 +30,10 @@ app.Post = Backbone.Model.extend({
     title: 'New post about Backbone',
     content: 'New post about Backbone content goes here'
   }
+});
+
+app.Posts = Backbone.Collection.extend({
+  model: app.Post
 });
 
 app.AppView = Backbone.View.extend({
