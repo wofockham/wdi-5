a6367ef7e5692278229fea5feff533879e0a37ce
Cold Head <certainthings@gmail.com>
2014-08-06 16:57:52 +1000

    is_expected.to

--- 06-advanced/fruitstore/db/schema.rb
+++ 06-advanced/fruitstore/db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140806025052) do
+ActiveRecord::Schema.define(version: 20140806044151) do
 
   create_table "fruits", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
     t.string   "type"
+    t.integer  "shelf_id"
   end
 
   create_table "shelves", force: true do |t|
