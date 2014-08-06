0e3dc9141c22f0b97221aad5720b39f81af9e3de
Cold Head <certainthings@gmail.com>
2014-08-06 10:47:07 +1000

    lol catholic church

--- 06-advanced/banking/bank.rb
+++ 06-advanced/banking/bank.rb
@@ -1,2 +1,37 @@
 class Bank
+  attr_reader :name, :accounts
+
+  def initialize(name)
+    @name = name
+    @accounts = {}
+  end
+
+  def create_account(account_name, deposit)
+    @accounts[account_name] = deposit
+  end
+
+  def deposit(account_name, amount)
+    @accounts[account_name] += amount
+  end
+
+  def balance(account_name)
+    @accounts[account_name]
+  end
+
+  def withdraw(account_name, amount) # Not an effective form of contraception
+    @accounts[account_name] -= amount
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 06-advanced/banking/spec/bank_spec.rb
+++ 06-advanced/banking/spec/bank_spec.rb
@@ -2,8 +2,61 @@ require_relative './spec_helper'
 require_relative '../bank'
 
 describe Bank do
+  let(:bank) {Bank.new('TDD Bank')}
+
   describe '.new' do
     it 'creates a new Bank object' do
+      expect(bank).to_not eq nil
+    end
+
+    it 'has a name' do
+      expect(bank.name).to eq 'TDD Bank'
+    end
+
+    it 'has no accounts' do
+      expect(bank.accounts.count).to eq 0
+    end
+  end
+
+  describe 'with an account' do
+    before(:each) {bank.create_account('Jimi', 200)}
+    describe '#create_account' do
+      it 'creates an account' do
+        expect(bank.accounts['Jimi']).to eq 200
+      end
+    end
+
+    describe '#deposit' do
+      it 'deposits money into the client\'s account' do
+        bank.deposit('Jimi', 300)
+        expect(bank.accounts['Jimi']).to eq 500
+      end
+    end
+
+    describe '#balance' do
+      it 'returns the balance for the client' do
+        expect(bank.balance('Jimi')).to eq 200
+      end
+    end
+
+    describe '#withdraw' do
+      it 'subtracts an amount from the client account' do
+        bank.withdraw('Jimi', 50)
+        expect(bank.balance('Jimi')).to eq 150
+      end
     end
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
