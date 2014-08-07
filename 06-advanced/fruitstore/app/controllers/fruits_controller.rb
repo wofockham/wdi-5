2689e4af885dad6611982f7a322b812c5002ab70
Cold Head <certainthings@gmail.com>
2014-08-07 11:46:41 +1000

    rich has a strange issue

--- 06-advanced/fruitstore/app/controllers/fruits_controller.rb
+++ 06-advanced/fruitstore/app/controllers/fruits_controller.rb
@@ -1,5 +1,10 @@
 class FruitsController < ApplicationController
   def index
     @fruits = Fruit.order('created_at DESC')
+
+    respond_to do |format|
+      format.html {}
+      format.json { render :json => @fruits }
+    end
   end
 end
--- 06-advanced/fruitstore/spec/controllers/fruits_controller_spec.rb
+++ 06-advanced/fruitstore/spec/controllers/fruits_controller_spec.rb
@@ -34,6 +34,26 @@ RSpec.describe FruitsController, :type => :controller do
       before do
         get :index, :format => :json
       end
+
+      it "should respond with a status 200" do
+        expect(response).to be_success
+        expect(response.status).to eq(200)
+      end
+
+      it "should give content type as JSON" do
+        expect(response.content_type).to eq('application/json')
+      end
+
+      it "should parse as valid JSON" do
+        # You are not expected to understand this.
+        expect( lambda { JSON.parse(response.body) } ).to_not raise_error
+      end
+
+      it "should have the name of the fruit in the JSON" do
+        fruits = JSON.parse(response.body)
+        expect(fruits.length).to eq(3)
+        expect(fruits.first["name"]).to eq("Fruit number 2")
+      end
     end
   end
 end
