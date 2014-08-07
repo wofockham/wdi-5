52828cbf682a5c0c401316eaeda04cf4190c871c
Cold Head <certainthings@gmail.com>
2014-08-07 14:50:11 +1000

    xmas steps

---
+++ 06-advanced/cuky/features/step_definitions/registration_steps.rb
@@ -0,0 +1,27 @@
+Given(/^I am on the sign up page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I sign up as a user with email "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should have an account$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I am on the login page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I have already registered as "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I login with correct information$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
